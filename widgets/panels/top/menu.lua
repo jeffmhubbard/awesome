56ac2ff06217c23b0484de626467f2950ad84681
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2021-05-14 11:54:44 -0500

    Add more customization to menu widget

--- widgets/panels/top/menu.lua
+++ widgets/panels/top/menu.lua
@@ -1,15 +1,19 @@
 local awful = require("awful")
 local theme = require("beautiful")
-local dpi = theme.xresources.apply_dpi
 local wibox = require("wibox")
 require("ui.menus")
 
--- theme.launcher_icon
+-- theme.menu_button_icon
+-- theme.menu_button_width
+-- theme.menu_button_fg
+-- theme.menu_button_bg
+-- theme.menu_button_border_color
+-- theme.menu_button_border_width
 -- theme.hover_color
 
 local menu_icon = wibox.widget {
     id = 'icon',
-    image = theme.launcher_icon,
+    image = theme.menu_button_icon,
     widget = wibox.widget.imagebox,
     resize = false,
 }
@@ -22,10 +26,14 @@ local menu_buttons = {
 local menu_widget = wibox.widget {
     {
         widget = menu_icon,
-        forced_width = dpi(36),
+        forced_width = theme.menu_button_width,
         valign = 'center',
         halign = 'center',
     },
+    fg = theme.menu_button_fg,
+    bg = theme.menu_button_bg,
+    border_color = theme.menu_button_border_color,
+    border_width = theme.menu_button_border_width,
     widget = wibox.container.background,
     buttons = menu_buttons,
 }
